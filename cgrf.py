e8c873f84c103cff876fc5bb16755a8d9059c7a9
BatuHanHub <114951572+BatuHanHub@users.noreply.github.com>
2022-12-27 23:04:30 +0300

    Update cgrf.py

--- cgrf.py
+++ cgrf.py
@@ -14,61 +14,83 @@ hosgeldiniz()
 while True:
     secim = str(input(">>>"))
     
-    if secim == "3":
-        input("çıkış")
+    # ÇIKIŞ +
+    if secim == "3": 
         break
     
+    #ÖLÇEKLER+
     elif secim == "1":
         os.system(system)
         olcekSecim()
         olcekTuru = str(input(">>>"))
         
         if olcekTuru == "1": 
-            input("Çizgili Olcek")
             os.system(system)
             print('\t=====ÇİZGİLİ ÖLÇEK=====\n')
+            print("FORMÜL: 1/x x'den 5 tane 0 silinir.\n")
             olcek = int(input("Ölceğinizi giriniz:"))
             if olcek >= 100000:
                 pass
             elif olcek < 100000:
-                olcekCizgiHata()
+                hataOlcekCizgi()
                 continue 
-            olcekCizgi()
-            print(olcek)
-            continue
+            olcek = olcek / 100000
+            
+            print(f'SONUÇ:\n1km={olcek}km')
+            input("\nÇıkmak için bir tuşa basınız...")
+            break
         
         elif olcekTuru == "2":
-            input("Kesirli olcek")
             os.system(system)
             print('\t=====KESİRLİ ÖLÇEK=====\n')
+            print("FORMÜL: x KM ye 5 tane 0 eklenir.\n")
             haritaUzunluk = 1
             gercekUzunluk = int(input("Gerçek uzunluğunuzu giriniz:"))
             olcek = gercekUzunluk * 100000
-            print(olcek)
+            print(f'SONUÇ:\n1/{olcek}')
+            input("\nÇıkmak için bir tuşa basınız...")
             continue
         
         else:
-            os.system(system)
             hataOlcekTuru()
             continue
-        
-        break
     
+    #HARİTA +
     elif secim == "2":
         os.system(system)
-        input('harita')
-        gercekUzunluk = int(input("Gerçek ucunluğunuz kaç kilometre:"))
-        haritaUzunluk = int(input("Haritanız kaç cm ile gösterilmiş:"))
-        gercekUzunluk = str(gercekUzunluk)
-        gercekUzunluk = gercekUzunluk + "00000"
-        gercekUzunluk = int(gercekUzunluk)
+        haritaSecim()
+        haritaTuru = str(input(">>>"))
+        if haritaTuru == "1":
+            os.system(system)
+            print('\t=====HARİTA ÖLÇEĞİ=====\n')
+            print('FORMÜL: gerçek uzunluğa 5 tane 0 eklenir. gerçek uzunluk/harita uzunluk\n')
+            gercekUzunluk = int(input("Gerçek uzunluğunuz kaç kilometre:"))
+            haritaUzunluk = int(input("Haritanız kaç cm ile gösterilmiş:"))
+            gercekUzunluk = gercekUzunluk * 100000
         
-        olcek = haritaUzunluk / gercekUzunluk
+            olcek = gercekUzunluk / haritaUzunluk
         
-        #çalışıyor EKLEME YAP
-        
-        print(olcek)
-        break
+            print(f'SONUÇ:\n1/{olcek}')
+            input("\nÇıkmak için bir tuşa basınız...")
+            break
+            
+        elif haritaTuru == "2":
+            os.system(system)
+            print('\t=====İKİ MERKEZ ARASI KM BULMA=====\n')
+            print('FORMÜL: Ölçeğe 5 tane 0 eklenir. Harita uzunluğu*Ölçek')
+            haritaUzunluk = int(input("Haritanız kaç cm ile gösterilmiş:"))
+            olcek = int(input("Ölçeğinizi giriniz:"))
+            
+            olcek = olcek * 100000
+            gercekUzunluk = haritaUzunluk * olcek
+            
+            print(f'SONUÇ:\n{gercekUzunluk}KM')
+            input("\nÇıkmak için bir tuşa basınız...")
+            break
+    
+        else: 
+            hataHaritaSecim()
+            continue
     
     else:
         os.system(sistem)
